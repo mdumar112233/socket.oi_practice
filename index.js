baf495f87f67a8372769c9764d58c03e5a5f7f6e
mdumar112233 <mdumarfaruk187326.com>
2021-08-21 23:11:39 +0600

    soket setup done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 // const http = require('http');
 // const expressServer = http.createServer(app);
 
-
+const {Server} = require('socket.io');
+const io = new Server(app);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
