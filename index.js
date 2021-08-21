c11e782c7cf33023550f3ac64ec991f05d9ed2d9
mdumar112233 <mdumarfaruk187326.com>
2021-08-21 23:23:54 +0600

    soket setup done

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-// const http = require('http');
-// const expressServer = http.createServer(app);
+const http = require('http');
+const expressServer = http.createServer(app);
 
 const {Server} = require('socket.io');
-const io = new Server(app);
+const io = new Server(expressServer);
+
+io.on('connection', (socket) => {
+    console.log('user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnect')
+    })
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
 })
 
-app.listen(3000)
+expressServer.listen(3000)
